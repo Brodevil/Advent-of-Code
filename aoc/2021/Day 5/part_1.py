d88ffd21e8049c86776b8869d22be2be61ad9525
Brodevil <brodevil89@gmail.com>
2021-12-05 19:03:26 +0530

    Abhinav Choudhary 10*

--- aoc/2021/Day 5/part_1.py
+++ aoc/2021/Day 5/part_1.py
@@ -4,18 +4,17 @@
 # Date = 5th of December 2021
 # Source =  [Advent Of Code](https://adventofcode.com/2021/day/5)
 
-# Solution : 
+# Solution :
 
 from collections import defaultdict
 
-infile = 'input.txt'
+ls = defaultdict(int)
 
-G1 = defaultdict(int)
-G2 = defaultdict(int)
-for line in open(infile):
-    start,end = line.split('->')
-    x1,y1 = start.split(',')
-    x2,y2 = end.split(',')
+for line in open("input.txt", "rt"):
+    start, end = line.split('->')
+    x1, y1 = start.split(',')
+    x2, y2 = end.split(',')
+    
     x1 = int(x1.strip())
     y1 = int(y1.strip())
     x2 = int(x2.strip())
@@ -24,13 +23,12 @@ for line in open(infile):
     dx = x2-x1
     dy = y2-y1
 
-    for i in range(1+max(abs(dx),abs(dy))):
-        x = x1+(1 if dx>0 else (-1 if dx<0 else 0))*i
-        y = y1+(1 if dy>0 else (-1 if dy<0 else 0))*i
-        if dx==0 or dy==0:
-            G1[(x,y)] += 1
-        G2[(x,y)] += 1
+    for i in range(1 + max(abs(dx), abs(dy))):
+        x = x1+(1 if dx > 0 else (-1 if dx < 0 else 0))*i
+        y = y1+(1 if dy > 0 else (-1 if dy < 0 else 0))*i
+        if dx == 0 or dy == 0:
+            ls[(x, y)] += 1
 
-print(len([k for k in G1 if G1[k]>1]))
-print(len([k for k in G2 if G2[k]>1]))
-# Answer : 
+print(len([k for k in ls if ls[k] > 1]))
+
+# Answer : 7318
--- aoc/2021/Day 5/part_2.py
+++ aoc/2021/Day 5/part_2.py
@@ -6,6 +6,37 @@
 
 # Solution : 
 
+# Advent of code 2021 : Day 5 | Part 1
+
+# Author = Abhinav
+# Date = 5th of December 2021
+# Source =  [Advent Of Code](https://adventofcode.com/2021/day/5)
+
+# Solution : 
+
+from collections import defaultdict
+
+ls = defaultdict(int)
+
+for line in open("input.txt", "rt"):
+    start,end = line.split('->')
+    x1,y1 = start.split(',')
+    x2,y2 = end.split(',')
+    
+    x1 = int(x1.strip())
+    y1 = int(y1.strip())
+    x2 = int(x2.strip())
+    y2 = int(y2.strip())
+
+    dx = x2-x1
+    dy = y2-y1
+
+    for i in range(1+max(abs(dx),abs(dy))):
+        x = x1+(1 if dx>0 else (-1 if dx<0 else 0))*i
+        y = y1+(1 if dy>0 else (-1 if dy<0 else 0))*i
+        ls[(x,y)] += 1
+
+print(len([k for k in ls if ls[k]>1]))
 
 
-# Answer : 
+# Answer : 19939
