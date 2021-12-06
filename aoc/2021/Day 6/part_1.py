5fa239167654760eed220e5d1b4eefd8357927fd
Brodevil <brodevil89@gmail.com>
2021-12-06 18:49:23 +0530

    Abhinav Choudhary 12*

--- aoc/2021/Day 6/part_1.py
+++ aoc/2021/Day 6/part_1.py
@@ -4,8 +4,28 @@
 # Date = 6th of December 2021
 # Source =  [Advent Of Code](https://adventofcode.com/2021/day/6)
 
-# Solution : 
+# Solution :
+
+from collections import defaultdict, Counter
+
+inputs = Counter([int(x) for x in open('aoc/2021/Day 6/input.txt').read().strip().split(',')])
 
 
+def solve(S, n):
+    inputs = S
+    for day in range(n):
+        input = defaultdict(int)
+        for x, cnt in inputs.items():
+            if x == 0:
+                input[6] += cnt
+                input[8] += cnt
+            else:
+                input[x-1] += cnt
+        inputs = input
+    return sum(inputs.values())
 
-# Answer : 
+
+print(solve(inputs, 80))
+
+
+# Answer : 351188
--- aoc/2021/Day 6/part_2.py
+++ aoc/2021/Day 6/part_2.py
@@ -6,6 +6,23 @@
 
 # Solution : 
 
+from collections import defaultdict, Counter
 
+inputs = Counter([int(x) for x in open('aoc/2021/Day 6/input.txt').read().strip().split(',')])
 
-# Answer : 
+def solve(S, n):
+    inputs = S
+    for day in range(n):
+        input = defaultdict(int)
+        for x, cnt in inputs.items():
+            if x == 0:
+                input[6] += cnt
+                input[8] += cnt
+            else:
+                input[x-1] += cnt
+        inputs = input
+    return sum(inputs.values())
+
+print(solve(inputs, 256))
+
+# Answer : 1595779846729
