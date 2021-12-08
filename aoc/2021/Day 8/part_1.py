3c97eb1fec7767ec0b6cb49612649fe75834fff9
Brodevil <brodevil89@gmail.com>
2021-12-08 20:33:35 +0530

    Abhinav Choudhary 15*

--- aoc/2021/Day 8/part_1.py
+++ aoc/2021/Day 8/part_1.py
@@ -4,8 +4,19 @@
 # Date = 8th of December 2021
 # Source =  [Advent Of Code](https://adventofcode.com/2021/day/8)
 
-# Solution : 
+# Solution :
 
+inputs = open("aoc/2021/Day 8/input.txt")
+inputs = inputs.read().splitlines()
 
+result = 0
 
-# Answer : 
+for line in inputs:
+    _, output = line.split(" | ")
+    output = output.split()
+
+    result += sum(len(_) in (2, 3, 4, 7) for _ in output)
+
+print(result)
+
+# Answer : 255
