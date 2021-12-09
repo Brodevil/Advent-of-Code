24aca9cd9741b307bd8ba2bd38a3fb8b27d06d06
Brodevil <brodevil89@gmail.com>
2021-12-09 19:57:02 +0530

    Abhinav Choudhary 4*

---
+++ aoc/2020/Day 2/part_1.py
@@ -0,0 +1,21 @@
+# Advent of code 2021 : Day 2 | Part 1
+# Source : https://adventofcode.com/2020/day/2
+
+infile = "aoc/2020/Day 2/input.txt"
+inputs = open(infile).read().splitlines()
+
+total = 0
+
+for _ in inputs:
+    _ = _.split()
+    word, letter = _[2], _[1][0]
+    lo, hi = map(int, _[0].split('-'))
+
+    c = word.count(letter)
+
+    if c >= lo and c <= hi:
+        total += 1
+
+print(total)
+
+# Answer : 460
---
+++ aoc/2020/Day 3/part_1.py
@@ -0,0 +1,5 @@
+# Advent of code 2021 : Day 3 | Part 1
+# Source : https://adventofcode.com/2020/day/3
+
+
+# Answer : 
