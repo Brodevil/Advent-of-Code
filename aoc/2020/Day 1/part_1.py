861f5a638ed5107266c8635b5b6714eec48c7ef6
Brodevil <brodevil89@gmail.com>
2021-12-09 19:03:29 +0530

    Intial Commit

--- aoc/2020/Day 1/part_1.py
+++ aoc/2020/Day 1/part_1.py
@@ -1,3 +1,9 @@
 # Advent of code 2021 : Day 1 | Part 1
 # Source : https://adventofcode.com/2020/day/1
 
+infile = "aoc/2020/Day 1/input.txt"
+inputs = list(map(int, open(infile).read().splitlines()))
+
+print([i * _ for _ in inputs for i in inputs if _ + i == 2020][0])
+
+# Answer : 157059
--- aoc/2020/Day 1/part_2.py
+++ aoc/2020/Day 1/part_2.py
@@ -1,3 +1,9 @@
 # Advent of code 2021 : Day 1 | Part 2
 # Source : https://adventofcode.com/2020/day/1
 
+infile = "aoc/2020/Day 1/input.txt"
+inputs = list(map(int, open(infile).read().splitlines()))
+
+print([i*j*_ for i in inputs for j in inputs for _ in inputs if i + j + _ == 2020][0])
+
+# Answer : 165080960
