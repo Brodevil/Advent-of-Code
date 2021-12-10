0f38e9050d9bc0b04627e4a815afe2fded55f268
Brodevil <brodevil89@gmail.com>
2021-12-10 18:38:08 +0530

    Abhinav Choudhary 20*

--- aoc/2021/Day 10/part_1.py
+++ aoc/2021/Day 10/part_1.py
@@ -6,5 +6,60 @@
 
 # Solution :
 
+from collections import deque
 
-# Answer :
+SCORES = []
+ans = 0
+infile = "aoc/2021/Day 10/input.txt"
+
+for line in open(infile):
+    bad = False
+    S = deque()
+    
+    for c in line.strip():
+        if c in ["(", "[", "{", "<"]:
+            S.append(c)
+        
+        elif c == ")":
+            if S[-1] != "(":
+                ans += 3
+                bad = True
+                break
+            else:
+                S.pop()
+        
+        elif c == "]":
+            if S[-1] != "[":
+                ans += 57
+                bad = True
+                break
+            else:
+                S.pop()
+        
+        elif c == "}":
+            if S[-1] != "{":
+                ans += 1197
+                bad = True
+                break
+            else:
+                S.pop()
+        
+        elif c == ">":
+            if S[-1] != "<":
+                ans += 25137
+                bad = True
+                break
+            else:
+                S.pop()
+    
+    if not bad:
+        score = 0
+        P = {"(": 1, "[": 2, "{": 3, "<": 4}
+        
+        for c in reversed(S):
+            score = score * 5 + P[c]
+        SCORES.append(score)
+
+print(ans)
+
+# Answer : 462693
--- aoc/2021/Day 10/part_2.py
+++ aoc/2021/Day 10/part_2.py
@@ -6,5 +6,59 @@
 
 # Solution : 
 
+from collections import deque
 
-# Answer : 
+SCORES = []
+ans = 0
+infile = "aoc/2021/Day 10/input.txt"
+
+for line in open(infile):
+    bad = False
+    S = deque()
+    
+    for c in line.strip():
+        if c in ["(", "[", "{", "<"]:
+            S.append(c)
+        elif c == ")":
+            if S[-1] != "(":
+                ans += 3
+                bad = True
+                break
+            else:
+                S.pop()
+        
+        elif c == "]":
+            if S[-1] != "[":
+                ans += 57
+                bad = True
+                break
+            else:
+                S.pop()
+        
+        elif c == "}":
+            if S[-1] != "{":
+                ans += 1197
+                bad = True
+                break
+            else:
+                S.pop()
+        
+        elif c == ">":
+            if S[-1] != "<":
+                ans += 25137
+                bad = True
+                break
+            else:
+                S.pop()
+    if not bad:
+        score = 0
+        P = {"(": 1, "[": 2, "{": 3, "<": 4}
+        for c in reversed(S):
+            score = score * 5 + P[c]
+        
+        SCORES.append(score)
+
+SCORES.sort()
+print(SCORES[len(SCORES) // 2])
+
+# Answer : 3094671161
