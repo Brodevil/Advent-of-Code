5d2d3fb260e93b1c8e9b79aeba89e93d530b2e9c
Brodevil <brodevil89@gmail.com>
2021-12-10 10:23:04 +0530

    Intial Commit

--- aoc/2020/Day 3/part_1.py
+++ aoc/2020/Day 3/part_1.py
@@ -2,4 +2,21 @@
 # Source : https://adventofcode.com/2020/day/3
 
 
+infile = "aoc/2020/Day 3/input.txt"
+inputs = open(infile).read().splitlines()
+
+trees = 0
+x, y = len(inputs[0]), len(inputs)
+X, Y = 0, 0
+right = True
+
+for _ in range(x):
+    if right:
+        X += 3
+    Y += 1
+    if inputs[Y][X] == "#":
+        trees += 1
+
+print(trees)
+
 # Answer : 
--- aoc/2020/Day 3/part_2.py
+++ aoc/2020/Day 3/part_2.py
@@ -1,5 +1,25 @@
-# Advent of code 2021 : Day 23 | Part 1
+# Advent of code 2021 : Day 3 | Part 1
 # Source : https://adventofcode.com/2020/day/23
 
+slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+
+G = []
+infile = "aoc/2020/Day 3/input.txt"
+G = open(infile).read().splitlines()
+
+ans = 1
+for (dc,dr) in slopes:
+    r = 0
+    c = 0 
+    score = 0
+    while r < len(G):
+        c += dc
+        r += dr
+        if r<len(G) and G[r][c%len(G[r])]=='#':
+            score += 1
+    ans *= score
+    if dc==3 and dr==1:
+        print(score)
+print(ans)
 
 # Answer : 
