2ebf3f0ca07f56dc43ee5adfda3d960ecbc91026
Brodevil <brodevil89@gmail.com>
2021-12-10 18:40:31 +0530

    Intial Commit

--- aoc/2021/Day 11/solution.py
+++ aoc/2021/Day 11/solution.py
@@ -1,10 +1,16 @@
-# Advent of code 2021 : Day 11 | Part 2
+# Advent of code 2021 : Day 11 | Part 1 & Part 2
 
 # Author = Abhinav
 # Date = 11th of December 2021
 # Source =  [Advent Of Code](https://adventofcode.com/2021/day/11)
 
-# Solution : 
+# Solution :
 
 
-# Answer : 
+# Part-1 :
+
+
+# Part-2 :
+
+
+# Answer :
---
+++ aoc/2021/Day 12/solution.py
@@ -0,0 +1,16 @@
+# Advent of code 2021 : Day 12 | Part 1 & Part 2
+
+# Author = Abhinav
+# Date = 12th of December 2021
+# Source =  [Advent Of Code](https://adventofcode.com/2021/day/12)
+
+# Solution :
+
+
+# Part-1 :
+
+
+# Part-2 :
+
+
+# Answer :
