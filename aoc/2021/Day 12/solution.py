b97c6874070bd0716abdde0f085c57d2a1418a91
Brodevil <brodevil89@gmail.com>
2021-12-12 18:17:33 +0530

    Abhinav Choudhary 24*

--- aoc/2021/Day 12/solution.py
+++ aoc/2021/Day 12/solution.py
@@ -6,6 +6,41 @@
 
 # Solution :
 
+infile = "aoc/2021/Day 12/input.txt"
+
+from collections import defaultdict, deque
+
+ls = defaultdict(list)
+for line in open(infile):
+    a, b = line.strip().split("-")
+    ls[a].append(b)
+    ls[b].append(a)
 
 
-# Answer :
+def solve(p1):
+    start = ("start", set(["start"]), None)
+    ans = 0
+    Q = deque([start])
+    
+    while Q:
+        pos, small, twice = Q.popleft()
+        if pos == "end":
+            ans += 1
+            continue
+        
+        for y in ls[pos]:
+            if y not in small:
+                new_small = set(small)
+                if y.lower() == y:
+                    new_small.add(y)
+                Q.append((y, new_small, twice))
+            
+            elif y in small and twice is None and y not in ["start", "end"] and not p1:
+                Q.append((y, small, y))
+    return ans
+
+
+print(solve(p1=True))
+print(solve(p1=False))
+
+# Answer : Part 1 - 3887 | Part 2 - 102834
---
+++ aoc/2021/Day 14/solution.py
@@ -0,0 +1,12 @@
+# Advent of code 2021 : Day 14 | Part 1 & Part 2
+
+# Author = Abhinav
+# Date = 14th of December 2021
+# Source =  [Advent Of Code](https://adventofcode.com/2021/day/14)
+
+# Solution :
+
+
+
+
+# Answer :
